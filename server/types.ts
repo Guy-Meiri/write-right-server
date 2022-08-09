b764b948e02f0dd3fba373f6a0fabfa40d5fc3ce
guymeiri <guymeiri@microsoft.com>
2022-08-09 23:46:55 +0300

    support coloredPixelsStatus in player progress

--- server/types.ts
+++ server/types.ts
@@ -1,6 +1,14 @@
 export interface ProgressStatus {
 	accuracy: number;
 	completionPercentage: number;
+	coloredPixelsStatus: ColoredPixelsStatus;
+}
+
+export interface ColoredPixelsStatus {
+	numPositivePoints: number;
+	numNegativePoints: number;
+	numMediumMistakePoints: number;
+	totalPoints: number;
 }
 
 export interface Player {
